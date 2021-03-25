6997f96dbdda2d2ec4835d21625fe8de730a98db
jon grabbe <jongrabbe@outlook.com>
2021-03-24 20:03:12 -0700

    added scoll transion to header

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-document.getElementById('shop-link').addEventListener('mouseover', () => {
-    document.getElementById('shop-container').classList.add('an-menu-up-vis')
-})
-document.getElementById('shop-link').addEventListener('mouseleave', () => {
-    document.getElementById('shop-container').classList.remove('an-menu-up-vis')
-})
+// document.getElementById('shop-link').addEventListener('mouseover', () => {
+//     document.getElementById('shop-container').classList.add('an-menu-up-vis')
+// })
+// document.getElementById('shop-link').addEventListener('mouseleave', () => {
+//     document.getElementById('shop-container').classList.remove('an-menu-up-vis')
+// })
+
+
+
+//scroll
+window.onscroll = function() {
+    let hasScrolled = document.getElementById('header').classList.contains('scroll-down');
+    if(scrollY > 0) {
+        document.getElementById('header').classList.add('scroll-down')
+        return
+    }
+    if(scrollY == 0 && hasScrolled) {
+        document.getElementById('header').classList.remove('scroll-down')
+    }
+}
